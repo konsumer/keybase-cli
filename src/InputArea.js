77121d6d5e4f2018d207a2b4d5dfe77f8858a1e7
David Konsumer <konsumer@jetboystudio.com>
2018-11-19 14:47:02 -0800

    auto-focus for text input

--- src/InputArea.js
+++ src/InputArea.js
@@ -4,13 +4,14 @@ import { sendMsg } from './keybase'
 import { withKeybase } from './keybase_context'
 
 class InputArea extends Component {
-  onSubmit = () => {
-    const body = this.text.value
+  onSubmit = (value) => {
+    const body = value
     this.text.clearValue()
     sendMsg({ options: { channel: { name: this.props.currentChannel.channel.name }, message: { body } } })
   }
 
   render () {
+    this.text && this.text.readInput()
     return (<textbox ref={r => { this.text = r }} onSubmit={this.onSubmit} keys mouse inputOnFocus {...this.props} />)
   }
 }
